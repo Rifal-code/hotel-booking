2e4f26c9805c9d67efb098b59d0711e87bf5dfef
RIfal-code <rifalghozali8@gmail.com>
2026-03-12 23:18:06 +0700

    feat(room): enhance create room form

--- app/admin/room/create/page.tsx
+++ app/admin/room/create/page.tsx
@@ -1,12 +1,11 @@
+import CreateForm from "@/components/admin/room/create-form"; 
 
-
-
-const CreateRoomPage = () =>  {
-    return (
-        <div className="max-w-screen px-4 py-16 mt-10 mx-auto">
-        
+const CreateRoomPage = () => {
+  return (
+    <div className="max-w-screen px-4 py-16 mt-10 mx-auto">
+      <CreateForm />
     </div>
-    )
-}
+  );
+};
 
-export default CreateRoomPage
+export default CreateRoomPage;
